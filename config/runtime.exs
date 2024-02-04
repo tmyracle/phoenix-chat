07bcf52d4feaac7bd7995bda005dc21269817e64
Tyler Myracle <tyler.myracle@gmail.com>
2024-02-03 19:52:06 -0600

    try build

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,13 +48,13 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || System.get_env("RENDER_EXTERNAL_HOSTNAME")
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :chat, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :chat, ChatWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
